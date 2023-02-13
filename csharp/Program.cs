1285aec70951608c3364fdbf7019af6d70ac0f3e
fantkolja <fantkolja@gmail.com>
2023-02-13 16:48:36 +0200

    add prototype sample

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -3,22 +3,6 @@ using ProgrammingPrinciples;
 using DesignPatterns.Prototype;
 using DesignPatterns.Protoype.ExtensionMethods;
 
-
-var photo = new Photo("photo-of-vasya.jpg");
-var idCard = new IDCard(photo);
-var vasya = new Person(new[] { "Vasyl", "Petrovych", "Petrenko" }, idCard);
-
-var kolya = (Person) vasya.DeepClone();
-// kolya.Names = new[] { "Mykola", "Mykolajovych", "Mykolenko" };
-kolya.Names.SetValue("Mykola", 0);
-kolya.Names.SetValue("Mykolajovych", 1);
-kolya.Names.SetValue("Mykolenko", 2);
-kolya.IDCard.ID = Guid.NewGuid();
-kolya.IDCard.Photo.Url = "photo-of-kolya.jpg";
-
-Console.WriteLine(vasya);
-Console.WriteLine(kolya);
-
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
 Console.SetCursorPosition(0, Console.CursorTop);
