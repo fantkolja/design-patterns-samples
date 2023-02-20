d646f5907f1df5198502178a8b5dbd00a19fce3a
fantkolja <fantkolja@gmail.com>
2023-02-20 17:44:22 +0200

    wip

--- csharp/src/patterns/State/GymTerminal/GymTerminal.cs
+++ csharp/src/patterns/State/GymTerminal/GymTerminal.cs
@@ -1,21 +1,31 @@
 /**
-* State matrix
+* Elevator state matrix
 * 
-* | State             | CustomerTouch          | FaceRecord            | FaceConfirmation | FaceReject             |
-* | :---:             | :---:                  | :---:                 | :---:            | :---:                  |
-* | EnterStandBy      | GOTO WaitingFaceRecord | Turn on Camera        | GOTO Open        | GOTO WaitingFaceRecord |
-* | ExitStandBy       | GOTO Open              | n/a                   |                  |                        |
-* | Open              | n/a (?)                |                       |                  |                        |
-* | WaitingFaceRecord | n/a (?)                | GOTO FaceConfirmation | n/a              |                        |
-* | Emergency         | n/a                    | n/a                   | n/a              |                        |
+* | State             | CalledFromOutside           | FloorButtonPressed          | CloseDoorsButtonPressed | OpenDoorsButtonPressed |
+* | :---:             | :---:                       | :---:                       | :---:                   | :---:                  |
+* | StayingClosed     | => StayingOpen || => Moving | => Moving || => StayingOpen | GOTO Open               | GOTO WaitingFaceRecord |
+* | StayingOpen       | noop || =>                  | n/a                         | n/a                     | n/a                    |
+* | Moving            | n/a                         | n/a                         | n/a                     | n/a                    |
+* | Emergency         | n/a                         | n/a                         | n/a                     | n/a                    |
 *
 */
 
 
+/**
+* GymTerminal state matrix
+* 
+* | State   \   Event | Braceletidentified          | BraceletRejected            | FaceIdentified          | FaceRejected           |
+* | :---------------: | :-------------------------: | :-------------------------: | :---------------------: | :--------------------: |
+* | EnterStandBy      | n/a                         | n/a                         | n/a                     | n/a                    |
+* | ExitStandBy       | n/a                         | n/a                         | n/a                     | n/a                    |
+* | CameraOn          | n/a                         | n/a                         | n/a                     | n/a                    |
+*
+*/
+
 
 namespace DesignPatterns.State
 {
-  class GymTerminal
+  class Elevator
   {
     
   }
