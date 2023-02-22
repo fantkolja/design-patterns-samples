10fd55890bc6509912b524e9947ae2b68b518ea8
fantkolja <fantkolja@gmail.com>
2023-02-22 13:45:20 +0200

    wip

---
+++ csharp/src/patterns/State/GitBucket/GitBucket.cs
@@ -0,0 +1,91 @@
+namespace DesignPatterns.State
+{
+  class PullRequest
+  {
+    public string State { get; set; } = "draft"; // "edit" "open" "merged" "closed"
+
+    public void OnCreated()
+    {
+      if (this.State == "draft")
+      {
+
+      }
+      else if (this.State == "open")
+      {
+
+      } 
+    }
+
+    public void OnSubmit()
+    {
+      if (this.State == "draft")
+      {
+
+      }
+      else if (this.State == "open")
+      {
+
+      } 
+    }
+  }
+}
+
+
+
+
+
+
+
+
+/**
+* Elevator state matrix
+* 
+* | State             | CalledFromOutside           | FloorButtonPressed          | CloseDoorsButtonPressed | OpenDoorsButtonPressed |
+* | :---:             | :---:                       | :---:                       | :---:                   | :---:                  |
+* | StayingClosed     | => StayingOpen || => Moving | => Moving || => StayingOpen | GOTO Open               | GOTO WaitingFaceRecord |
+* | StayingOpen       | noop || =>                  | n/a                         | n/a                     | n/a                    |
+* | Moving            | n/a                         | n/a                         | n/a                     | n/a                    |
+* | Emergency         | n/a                         | n/a                         | n/a                     | n/a                    |
+*
+*/
+
+
+/**
+* GymTerminal state matrix
+* 
+* | State   \   Event | BraceletIdentified          | BraceletRejected            | FaceIdentified          | FaceRejected           |
+* | :---------------: | :-------------------------: | :-------------------------: | :---------------------: | :--------------------: |
+* | EnterStandBy      | n/a                         | n/a                         | n/a                     | n/a                    |
+* | ExitStandBy       | n/a                         | n/a                         | n/a                     | n/a                    |
+* | CameraOn          | n/a                         | n/a                         | n/a                     | n/a                    |
+*
+*/
+
+
+  // class Elevator
+  // {
+  //   public enum State
+  //   {
+  //     MovingUp,
+  //     MovingDown,
+  //     Standing,
+  //   }
+  //   public State CurrentState { get; set; } = State.Standing;
+  //   public int CurrentFloor { get; set; } = 1;
+  //   public int CurrentDestination { get; set; } = 1;
+
+  //   public void OnUpButtonPress(int floor)
+  //   {
+  //     if (this.CurrentState == State.MovingUp)
+  //     {
+  //       if (floor > this.CurrentFloor && floor < this.CurrentDestination) 
+  //       {
+  //         Console.WriteLine($"Stop on floor {floor} then on {this.CurrentDestination}");
+  //       }
+  //     }
+  //   }
+  //   public void OnDownButtonPress(int floor)
+  //   {
+      
+  //   }
+  // }
