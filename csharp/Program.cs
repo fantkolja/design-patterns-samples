0ae579f9c7f5cc750e23feef081496bc4b0aae14
fantkolja <fantkolja@gmail.com>
2023-04-24 10:27:21 +0300

    add wordo

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,24 +1,4 @@
-﻿using DesignPatterns.Iterator;
-
-var aggregate = new ConcreteAggregate();
-var iterator = aggregate.GetIterator();
-logEach(iterator);
-
-static void logEach(IClassicIterator iterator)
-{
-  while(!iterator.IsDone())
-  {
-    Console.WriteLine(iterator.Next());
-  };
-}
-
-var builtInList = new List<string>(){ "1", "2", "3", "4", "5" };
-var builtInIterator = builtInList.GetEnumerator();
-
-while(builtInIterator.MoveNext())
-{
-  Console.WriteLine(builtInIterator.Current);
-};
+﻿
 
 Console.WriteLine("");
 Console.WriteLine("Press any key to exit...");
