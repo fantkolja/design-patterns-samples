3534db794836264ccd1ba8fb2f1c4b13605a8289
fantkolja <fantkolja@gmail.com>
2023-04-30 11:29:23 +0300

    add js iterator

--- js/src/patterns/Iterator/index.js
+++ js/src/patterns/Iterator/index.js
@@ -1,5 +1,62 @@
 const arr = [1, 2, 3];
 
-for (let n of arr) {
-  console.log(n);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Range {
+//   constructor(from, to) {
+//     this.from = from;
+//     this.to = to;
+//   }
+
+//   [Symbol.iterator]() {
+//     let current = null;
+
+//     return {
+//       next: () => {
+//         if (current === null) {
+//           current = this.from;
+//         } else {
+//           current++;
+//         }
+//         return current <= this.to ? {
+//           value: current,
+//           done: false,
+//         } : {
+//           value: undefined,
+//           done: true,
+//         };
+//       }};
+//   }
+// };
+
+
+// const range = new Range(1, 3);
+
+
+// console.log([...range]);
+// console.log(...range);
