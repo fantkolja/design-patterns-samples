a1cf8fb55470ab07f9b5ec4ec8a929443cf23f78
Mykola Fant <mykola.fant@hazelcast.com>
2024-02-26 13:07:28 +0200

    remove serialization

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,17 +1,4 @@
-﻿using DesignPatterns.Prototype;
-
-var photo = new Photo("https://cool-photo");
-var idCard = new IDCard(photo);
-var mykola = new Person(new[] { "Mykola", "Fant" }, idCard);
-
-Console.WriteLine(mykola);
-
-var clonoKolya = (Person) mykola.Clone();
-clonoKolya.Names[0] = "Kolya";
-
-Console.WriteLine(clonoKolya);
-Console.WriteLine(mykola);
-
+﻿
 Console.WriteLine();
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
