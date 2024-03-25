cacb6e629b6c884024ad4a65df58735154d7d025
Mykola Fant <mykola.fant@hazelcast.com>
2024-03-25 13:08:17 +0200

    enhance adapter

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,11 +1,4 @@
-﻿using DesignPatterns.Adapter;
-
-var smallShape = new Cylinder(5);
-var bigShape = new Cylinder(8);
-var hole = new RoundHole(6);
-
-Console.WriteLine(hole.Fits(smallShape));
-Console.WriteLine(hole.Fits(bigShape));
+﻿
 
 Console.WriteLine();
 Console.WriteLine("Press any key to exit...");
