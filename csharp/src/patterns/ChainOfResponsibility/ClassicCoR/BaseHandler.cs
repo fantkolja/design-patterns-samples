37d9f647608c35d930d929da0fdb38905b100772
Mykola Fant <mykola.fant@hazelcast.com>
2024-04-07 22:46:19 +0300

    better cor

--- csharp/src/patterns/ChainOfResponsibility/ClassicCoR/BaseHandler.cs
+++ csharp/src/patterns/ChainOfResponsibility/ClassicCoR/BaseHandler.cs
@@ -1,14 +1,15 @@
 namespace DesignPatterns.ChainOfResponsibility
 {
-  abstract class BaseHandler : IHandler
+  abstract class BaseHandler
   {
-    private IHandler? _next;
+    private BaseHandler? _next;
 
-    public IHandler SetNextHandler(IHandler next)
+    public BaseHandler SetNextHandler(BaseHandler next)
     {
       this._next = next;
 
       // similiar to fluent interface
+      // or to a Singly Linked List
       return next;
     }
 
@@ -29,9 +30,9 @@ namespace DesignPatterns.ChainOfResponsibility
 // using DesignPatterns.ChainOfResponsibility;
 
 
-// var handler1 = new ConcreteHandler1();
-// var handler2 = new ConcreteHandler2();
+// BaseHandler handler1 = new ConcreteHandler1();
+// BaseHandler handler2 = new ConcreteHandler2();
 
 // handler1.SetNextHandler(handler2);
 
-// handler2.Handle(4);
+// handler1.Handle(7);
