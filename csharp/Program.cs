ada6beacb4d6afe511867ce6fc39d55b5c402161
Mykola Fant <mykola.fant@hazelcast.com>
2025-03-25 17:45:36 +0200

    add manual canary release

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,9 +1,4 @@
-﻿using DesignPatterns.Mediator;
-
-Component1 component1 = new Component1("Component 1");
-Component2 component2 = new Component2("Component 2");
-
-new Mediator(component1, component2);
+﻿
 
 Console.WriteLine();
 Console.WriteLine("Press any key to exit...");
