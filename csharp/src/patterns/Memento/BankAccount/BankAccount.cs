001eb06bc9f2e9da8400db2b6b47725340a935ae
fantkolja <fantkolja@gmail.com>
2026-04-01 21:42:21 +0300

    migrate Button

--- csharp/src/patterns/Memento/BankAccount/BankAccount.cs
+++ csharp/src/patterns/Memento/BankAccount/BankAccount.cs
@@ -1,9 +1,7 @@
-// naive implementation
 namespace DesignPatterns.Memento
 {
   class BankAccount
   {
-    private List<BankAccount> _history = new List<BankAccount>();
     private double _amount;
     private string _currency;
 
@@ -11,7 +9,6 @@ namespace DesignPatterns.Memento
     {
       this._amount = initialAmount;
       this._currency = currency;
-      SaveToHistory();
       Console.WriteLine($"Account created with initial amount {initialAmount}{currency}");
     }
 
@@ -23,7 +20,7 @@ namespace DesignPatterns.Memento
       }
       this._amount += amountChange;
       if (amountChange > 0) {
-        Console.WriteLine($"Deposited {amountChange}{this._currency}; balance is now {this._amount}{this._currency}");  
+        Console.WriteLine($"Deposited {amountChange}{this._currency}; balance is now {this._amount}{this._currency}");
       } else {
         Console.WriteLine($"Withdrew {amountChange * -1}{this._currency}; balance is now {this._amount}{this._currency}");
       }
@@ -43,25 +40,74 @@ namespace DesignPatterns.Memento
     {
       return $"{this._amount} {this._currency}";
     }
-
-    public int SaveToHistory()
-    {
-      this._history.Add((BankAccount)this.MemberwiseClone());
-      return this._history.Count - 1;
-    }
-
-    public void Restore(int index)
-    {
-      BankAccount snapshot = this._history.ElementAt(index);
-      this._history.RemoveRange(index, this._history.Count - index);
-      this._amount = snapshot._amount;
-      this._currency = snapshot._currency;
-    }
-
-    public void Undo() {  }
-    public void PrintTransactions(){  }
   }
 }
+
+
+// naive implementation
+// namespace DesignPatterns.Memento
+// {
+//   class BankAccount
+//   {
+//     private List<BankAccount> _history = new List<BankAccount>();
+//     private double _amount;
+//     private string _currency;
+//
+//     public BankAccount(double initialAmount, string currency)
+//     {
+//       this._amount = initialAmount;
+//       this._currency = currency;
+//       SaveToHistory();
+//       Console.WriteLine($"Account created with initial amount {initialAmount}{currency}");
+//     }
+//
+//     public double Deposit(double amountChange)
+//     {
+//       if (amountChange == 0)
+//       {
+//         throw new ArgumentException("Transactions with 0 sum are not allowed!");
+//       }
+//       this._amount += amountChange;
+//       if (amountChange > 0) {
+//         Console.WriteLine($"Deposited {amountChange}{this._currency}; balance is now {this._amount}{this._currency}");
+//       } else {
+//         Console.WriteLine($"Withdrew {amountChange * -1}{this._currency}; balance is now {this._amount}{this._currency}");
+//       }
+//       SaveToHistory();
+//       return this._amount;
+//     }
+//
+//     public void ChangeCurrency(string to)
+//     {
+//       double changeCourse = new Random().NextDouble();
+//       this._currency = to;
+//       this._amount = Math.Round(this._amount * changeCourse, 2);
+//       SaveToHistory();
+//     }
+//
+//     public override string ToString()
+//     {
+//       return $"{this._amount} {this._currency}";
+//     }
+//
+//     public int SaveToHistory()
+//     {
+//       this._history.Add((BankAccount)this.MemberwiseClone());
+//       return this._history.Count - 1;
+//     }
+//
+//     public void Restore(int index)
+//     {
+//       BankAccount snapshot = this._history.ElementAt(index);
+//       this._history.RemoveRange(index, this._history.Count - index);
+//       this._amount = snapshot._amount;
+//       this._currency = snapshot._currency;
+//     }
+//
+//     public void Undo() {  }
+//     public void PrintTransactions(){  }
+//   }
+// }
 
 // using DesignPatterns.Memento;
 
@@ -113,7 +159,7 @@ namespace DesignPatterns.Memento
 //   class AccountSnapshotManager
 //   {
 //     private List<IBankAccountSnapshot> _history = new List<IBankAccountSnapshot>();
-    
+//
 //     public void Save(IBankAccountSnapshot snapshot)
 //     {
 //       this._history.Add(snapshot);
@@ -136,13 +182,13 @@ namespace DesignPatterns.Memento
 //   {
 //     private double _amount;
 //     private string _currency;
-
+//
 //     public BankAccount(double initialAmount, string currency)
 //     {
 //       this._amount = initialAmount;
 //       this._currency = currency;
 //     }
-
+//
 //     public double Deposit(double amountChange)
 //     {
 //       if (amountChange == 0)
@@ -151,30 +197,30 @@ namespace DesignPatterns.Memento
 //       }
 //       this._amount += amountChange;
 //       if (amountChange > 0) {
-//         Console.WriteLine($"Deposited {amountChange}{this._currency}; balance is now {this._amount}{this._currency}");  
+//         Console.WriteLine($"Deposited {amountChange}{this._currency}; balance is now {this._amount}{this._currency}");
 //       } else {
 //         Console.WriteLine($"Withdrew {amountChange * -1}{this._currency}; balance is now {this._amount}{this._currency}");
 //       }
 //       return this._amount;
 //     }
-
+//
 //     public void ChangeCurrency(string to)
 //     {
 //       double changeCourse = new Random().NextDouble();
 //       this._currency = to;
 //       this._amount = Math.Round(this._amount * changeCourse, 2);
 //     }
-
+//
 //     public IBankAccountSnapshot CreateSnapshot()
 //     {
 //       return new BankAccountSnapshot(this);
 //     }
-
+//
 //     public override string ToString()
 //     {
 //       return $"{this._amount} {this._currency}";
 //     }
-
+//
 //     private class BankAccountSnapshot : IBankAccountSnapshot
 //     {
 //       public Guid Id { get; } = Guid.NewGuid();
@@ -188,7 +234,7 @@ namespace DesignPatterns.Memento
 //         this._currency = account._currency;
 //         this._originalAccount = account;
 //       }
-
+//
 //       public void Restore()
 //       {
 //         this._originalAccount._amount = this._amount;
@@ -200,11 +246,11 @@ namespace DesignPatterns.Memento
 
 
 // using DesignPatterns.Memento;
-
+//
 // var myAccount = new BankAccount(100, "UAH");
 // var manager = new AccountSnapshotManager();
 // manager.Save(myAccount.CreateSnapshot());
-
+//
 // myAccount.Deposit(100);
 // manager.Save(myAccount.CreateSnapshot());
 // myAccount.Deposit(200);
@@ -214,8 +260,8 @@ namespace DesignPatterns.Memento
 // myAccount.ChangeCurrency("USD");
 // myAccount.Deposit(20);
 // manager.Save(myAccount.CreateSnapshot());
-
+//
 // Console.WriteLine($"Current account: {myAccount}");
-
+//
 // manager.Restore(3);
 // Console.WriteLine($"Restored account: {myAccount}");
